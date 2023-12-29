dc2a1728c4add7f77a4c9142504e5e92fb10f243
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-12-29 13:25:24 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+import React from 'react';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  const { followers, views, likes } = stats;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="User avatar"
+          className="avatar"
+        />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
     </div>
   );
 };
+
+export default Profile;
