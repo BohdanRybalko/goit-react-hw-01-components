f603a21e8f41f5ca2c5ea5a8d9465eb61127fce2
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-07 23:31:41 +0200

    fix color

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusColor = isOnline ? 'green' : 'red';
+
   return (
     <li className="item">
-      <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
+      <span style={{ backgroundColor: statusColor }} className="status"></span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
     </li>
